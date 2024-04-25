772e6ad346ac2b0adb04bebee39cc9836521aea8
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-04-25 22:43:00 +0530

    Flowbite  talwind css framework added

--- client/src/components/ALLSports/Football/Competitions/Leagues/Leagues.js
+++ client/src/components/ALLSports/Football/Competitions/Leagues/Leagues.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Leagues() {
   return (
     <div>
-      <h1> This is Leaugues</h1>
+    <div className='text-blue-500'>
+    Hello 
+    </div>
     </div>
   )
 }
