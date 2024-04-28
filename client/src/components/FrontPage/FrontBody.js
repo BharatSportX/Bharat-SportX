d5c47d307d845dd81bcf108987c593a63225572c
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-28 17:47:52 +0530

    okkk

--- client/src/components/FrontPage/FrontBody.js
+++ client/src/components/FrontPage/FrontBody.js
@@ -6,13 +6,11 @@ const FrontBody = () => {
       <div class="flex flex-col md:flex-row mx-3 items-center">
         <div class="md:w-7/12 md:order-1">
           <h2 class="text-2xl font-normal leading-normal mb-2">
-            Other Places{" "}
+            LOra Banchod place{" "}
             <span class="text-gray-600">It'll also blow your mind.</span>
           </h2>
           <p class="text-lg leading-relaxed mb-4">
-            Here we add some another beautiful but this is desert like place so,
-            those are most beautiful because totally peace and calm you find
-            there.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim assumenda at inventore et odit esse incidunt sit molestias corporis. Dignissimos!
           </p>
         </div>
         <div class="md:w-5/12 md:order-2">
