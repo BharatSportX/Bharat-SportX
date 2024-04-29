898a3a9c058b55c412bd1eb6a0d1143fd56f9e4c
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-30 02:57:57 +0530

    changes

--- client/src/components/FrontPage/FrontBody/BodyQN.js
+++ client/src/components/FrontPage/FrontBody/BodyQN.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
 
-const BodyQN = () => {
+const BodyQN = (props) => {
+  const {qn,ans,targetAns,targetQn,body,isOpen,toggleAccordion}=props
   return (
-    <div>
-      my qn
-    </div>
-  )
-}
+    <>
+      {/* Accordings */}
+      <div  data-accordion="collapse" className="m-2 mx-6 lg:mx-44">
+        <h2 id={`${targetQn}`}>
+          <button
+            type="button"
+            className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white  border-gray-200 rounded-t-lg   hover:bg-gray-100 dark:hover:bg-gray-800 gap-3 bg-gray-800"
+            onClick={() => toggleAccordion(targetAns)}
+            aria-expanded={isOpen ? "true" : "false"}
+            aria-controls={body}
+          >
+            <span className="text-white">{qn}</span>
+            <svg
+              data-accordion-icon
+              className={`w-3 h-3 transform ${isOpen ? "" : "rotate-180"} transition-transform`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id={`${targetAns}`}
+          className={`${isOpen ? "" : "hidden"}`}//here add animation
+          aria-labelledby={`${targetQn}`}
+        >
+          <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+             {ans}
+            </p>
+            {/* <p className="text-gray-500 dark:text-gray-400">
+              Check out this guide to learn how to{" "}
+              <a
+                href="/docs/getting-started/introduction/"
+                className="text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                get started
+              </a>{" "}
+              and start developing websites even faster with components on top
+              of Tailwind CSS.
+            </p> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default BodyQN
+export default BodyQN;
--- client/src/components/FrontPage/FrontBody/FrontBody.js
+++ client/src/components/FrontPage/FrontBody/FrontBody.js
@@ -1,18 +1,106 @@
-import React from 'react'
-import BodyCard from './BodyCard'
-import BodyQN from './BodyQN'
+import React, { useState } from "react";
+import BodyCard from "./BodyCard";
+import BodyQN from "./BodyQN";
 
 const FrontBody = () => {
-  return (
-    <>
-    
-       <BodyCard paragraphorder="1" imgorder="2" img="https://source.unsplash.com/random/500x450/?football"/>
-      <BodyCard paragraphorder="2" imgorder="1" img="https://source.unsplash.com/random/500x450/?cricket"/>
-      <BodyCard paragraphorder="1" imgorder="2" img="https://source.unsplash.com/random/500x450/?chess"/>
-      <BodyCard paragraphorder="2" imgorder="1" img="https://source.unsplash.com/random/500x450/?basketball"/>
-      <BodyQN/>
-    </>
-  )
-}
+  const [accordionState, setAccordionState] = useState({});
 
-export default FrontBody
+  const toggleAccordion = (targetAns) => {
+    setAccordionState((prevState) => {
+      // Close any other open accordions
+      const newState = {};
+      for (const key in prevState) {
+        if (key !== targetAns) {
+          newState[key] = false;
+        }
+      }
+      // Toggle the clicked accordion
+      return { ...newState, [targetAns]: !prevState[targetAns] || false };
+    });
+  };
+  return (
+    <div className="">
+      <BodyCard
+        paragraphorder="1"
+        imgorder="2"
+        img="https://source.unsplash.com/random/500x450/?football"
+      />
+      <BodyCard
+        paragraphorder="2"
+        imgorder="1"
+        img="https://source.unsplash.com/random/500x450/?cricket"
+      />
+      <BodyCard
+        paragraphorder="1"
+        imgorder="2"
+        img="https://source.unsplash.com/random/500x450/?chess"
+      />
+      <BodyCard
+        paragraphorder="2"
+        imgorder="1"
+        img="https://source.unsplash.com/random/500x450/?basketball"
+      />
+      <div className="bg-black">
+        <h1 className=" text-4xl p-11 leading-snug lg:text-5xl text-white text-center pt-12 pb-6 font-[Rowdies,sans-serif] lg:font-bold">
+          Special Advance Features{" "}
+        </h1>
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="Firstans"
+          targetQn="Firstqn"
+          body="body1"
+           isOpen={accordionState["Firstans"]}
+           toggleAccordion={toggleAccordion}
+        />
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="Secondans"
+          targetQn="Secondqn"
+          body="body2"
+           isOpen={accordionState["Secondans"]}
+           toggleAccordion={toggleAccordion}
+        />
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="Thirdans"
+          targetQn="Thirdqn"
+          body="body3"
+           isOpen={accordionState["Thirdans"]}
+           toggleAccordion={toggleAccordion}
+        />
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="fourthans"
+          targetQn="fourthqn"
+          body="body4"
+           isOpen={accordionState["fourthans"]}
+           toggleAccordion={toggleAccordion}
+        />
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="fifthans"
+          targetQn="fifthqn"
+          body="body5"
+           isOpen={accordionState["fifthans"]}
+           toggleAccordion={toggleAccordion}
+        />
+        <BodyQN
+          qn="What is Special in BharatSportX"
+          ans=" You find your favourite sports here ans also you get live train with special branch "
+          targetAns="Sixthans"
+          targetQn="Sixthqn"
+          body="body6"
+           isOpen={accordionState["Sixthans"]}
+           toggleAccordion={toggleAccordion}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FrontBody;
