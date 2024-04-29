5708f5da419bca543c88f02efcd0b51b12672d5f
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-04-29 22:08:21 +0530

    identation added

--- client/src/components/ALLSports/Football/Competitions/Leagues/CupPageDetails.js
+++ client/src/components/ALLSports/Football/Competitions/Leagues/CupPageDetails.js
@@ -153,23 +153,23 @@ const CupPageDetails = () => {
         </div>
 
         <div className="flex flex-wrap justify-center">
-  {cupData.map(session => (
-    <div key={session.year} className="m-2">
-      <button onClick={() => handleShowFixture(session.year)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Session {session.year}</button>
-    </div>
-  ))}
-</div>
+          {cupData.map(session => (
+            <div key={session.year} className="m-2">
+              <button onClick={() => handleShowFixture(session.year)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Session {session.year}</button>
+            </div>
+          ))}
+        </div>
 
-{!loading && (
-  <div class="relative max-w-sm">
-    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-      <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-      </svg>
-    </div>
-    <input datepicker datepicker-buttons datepicker-autoselect-today type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"/>
-  </div>
-)}
+        {!loading && (
+          <div class="relative max-w-sm">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+              </svg>
+            </div>
+            <input datepicker datepicker-buttons datepicker-autoselect-today type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+          </div>
+        )}
 
         {/* <div class="relative max-w-sm">
   <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -182,9 +182,9 @@ const CupPageDetails = () => {
 
       </div>
       <div className="fixtures-container">
-      
+
         <p className="font-semibold">{selectedSessionYear}</p>
-        
+
 
         {sortedFixtures.map((fixture, index) => (
           <div key={fixture.fixture.id} className="fixture-card bg-gray-200 rounded-lg shadow-md p-4 my-4">
@@ -202,7 +202,7 @@ const CupPageDetails = () => {
                 <p>VS</p>
               </div>
               <div className="team flex items-center">
-               
+
                 <img className="w-8 h-8 rounded-full" src={fixture.teams.away.logo} alt={fixture.teams.away.name} />
                 <p className="ml-2">{fixture.teams.away.name}</p>
               </div>
