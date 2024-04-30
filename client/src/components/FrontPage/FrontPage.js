0ff2e6665cd8353847f3a76e09d263d8732592fe
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-30 18:52:21 +0530

    chanGE1

--- client/src/components/FrontPage/FrontPage.js
+++ client/src/components/FrontPage/FrontPage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./FrontPage.css";
 import FrontNavbar from "./FrontNavbar";
 import { Link } from "react-router-dom";
-import BodyCard from "./FrontBody/BodyCard";
 import FrontBody from "./FrontBody/FrontBody";
 
 const FrontPage = () => {
