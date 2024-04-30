09396032ec7519d9512e905a9b61a47bcdbd192b
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-30 20:35:15 +0530

    some change

--- client/src/components/FrontPage/Footer.js
+++ client/src/components/FrontPage/Footer.js
@@ -61,7 +61,7 @@ const Footer = () => {
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2021-2022{" "}
             <a href="#" class="hover:underline">
-              Flowbite™
+              BharatSportX™
             </a>
             . All Rights Reserved.
           </span>
