38ce22c9d73f3b4a912ddd1fd85f0001cdc5d26b
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-02 18:38:42 +0530

    updated

--- client/src/components/ALLSports/Football/Competitions/Leagues/Cup.js
+++ client/src/components/ALLSports/Football/Competitions/Leagues/Cup.js
@@ -13,7 +13,7 @@ function Cups() {
   const [isListening, setIsListening] = useState(false);
   const [isDanger, setIsDanger] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
-  const [isSearchBox,setSearchBox]=useState(false);
+  const [isSearchBox, setSearchBox] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -44,88 +44,88 @@ function Cups() {
   };
 
   const handleVoiceSearch = () => {
-  const recognition = new window.webkitSpeechRecognition();
-  let timeoutId;
+    const recognition = new window.webkitSpeechRecognition();
+    let timeoutId;
 
-  recognition.lang = 'en-IN'; // Set recognition language
+    recognition.lang = 'en-IN'; // Set recognition language
 
-  recognition.onstart = () => {
-    setIsListening(true);
-    setShowSpeakNow(true);
-    timeoutId = setTimeout(() => {
-      recognition.stop();
+    recognition.onstart = () => {
+      setIsListening(true);
+      setShowSpeakNow(true);
+      timeoutId = setTimeout(() => {
+        recognition.stop();
+        setShowSpeakNow(false);
+        setIsListening(false);
+      }, 6000);
+    };
+
+    recognition.onresult = (event) => {
+      clearTimeout(timeoutId);
+      const speechResult = event.results[0][0].transcript;
+      setSearchQuery(speechResult); // Set the search query to the recognized speech
+      // Handle recognition result...
+    };
+
+    recognition.onend = () => {
+      setShowSpeakNow(false); // Close modal when speech ends
+      setIsListening(false);
+    };
+
+    recognition.onerror = () => {
+      clearTimeout(timeoutId);
       setShowSpeakNow(false);
       setIsListening(false);
-    }, 6000);
-  };
-
-  recognition.onresult = (event) => {
-    clearTimeout(timeoutId);
-    const speechResult = event.results[0][0].transcript;
-    setSearchQuery(speechResult); // Set the search query to the recognized speech
-    // Handle recognition result...
-  };
-
-  recognition.onend = () => {
-    setShowSpeakNow(false); // Close modal when speech ends
-    setIsListening(false);
-  };
-
-  recognition.onerror = () => {
-    clearTimeout(timeoutId);
-    setShowSpeakNow(false);
-    setIsListening(false);
-    setIsDanger(true);
-    setIsSuccess(false);
-    setTimeout(() => {
-      setIsDanger(false);
-    }, 3000);
-  };
-
-  recognition.start();
-};
-
-  
-
-useEffect(() => {
-  handleSearch(); // Call handleSearch on every render
-});
-
-const handleSearch = () => {
-  // Perform search process
-  // For now, I'm just logging the searchQuery
-  // console.log('Search query:', searchQuery);
-  if (searchQuery) {
-    const filtered = cups.filter((cup) =>
-      cup.league.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    if (!searchQuery) {
-      setSearchBox(true);
-      return;
-    }
-    if (filtered.length === 0) {
-      // No result found, show danger alert
       setIsDanger(true);
       setIsSuccess(false);
-      //Set search box suggestion False
-      setSearchBox(false)
       setTimeout(() => {
         setIsDanger(false);
       }, 3000);
-    } else {
-      // Result found, show success alert
-      setIsDanger(false);
-      setIsSuccess(true);
-      //Show Search suggestion box
-      setSearchBox(true)
-      setTimeout(() => {
+    };
+
+    recognition.start();
+  };
+
+
+
+  useEffect(() => {
+    handleSearch(); // Call handleSearch on every render
+  });
+
+  const handleSearch = () => {
+    // Perform search process
+    // For now, I'm just logging the searchQuery
+    // console.log('Search query:', searchQuery);
+    if (searchQuery) {
+      const filtered = cups.filter((cup) =>
+        cup.league.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      if (!searchQuery) {
+        setSearchBox(true);
+        return;
+      }
+      if (filtered.length === 0) {
+        // No result found, show danger alert
+        setIsDanger(true);
         setIsSuccess(false);
-      }, 3000);
+        //Set search box suggestion False
+        setSearchBox(false)
+        setTimeout(() => {
+          setIsDanger(false);
+        }, 3000);
+      } else {
+        // Result found, show success alert
+        setIsDanger(false);
+        setIsSuccess(true);
+        //Show Search suggestion box
+        setSearchBox(true)
+        setTimeout(() => {
+          setIsSuccess(false);
+        }, 3000);
+      }
+      // Clear search query after displaying alerts
+
     }
-    // Clear search query after displaying alerts
-   
-  }
-};
+  };
 
 
 
@@ -144,7 +144,7 @@ const handleSearch = () => {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-4">Cups</h1>
-      
+
       {/* Speak Now Div */}
       {showSpeakNow && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -152,7 +152,13 @@ const handleSearch = () => {
             <p className="text-xl font-semibold mb-4">Speak Now</p>
             <p className="mb-4">Speak into the microphone...</p>
             {isListening && (
-              <div className="w-4 h-4 bg-red-500 rounded-full animate-ping"></div>
+
+              <div className="w-4 h-4 bg-red-500 rounded-full animate-ping">
+
+              </div>
+
+
+
             )}
           </div>
         </div>
@@ -166,7 +172,7 @@ const handleSearch = () => {
           handleSearch();
         }}
       > */}
-        {/* <label htmlFor="voice-search" className="sr-only">
+      {/* <label htmlFor="voice-search" className="sr-only">
           Search
         </label>
         <input
@@ -178,9 +184,9 @@ const handleSearch = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           required
         /> */}
-        
-        {/* Clear button */}
-        {/* {searchQuery && (
+
+      {/* Clear button */}
+      {/* {searchQuery && (
           <button
             type="button"
             onClick={handleClearSearch}
@@ -198,8 +204,8 @@ const handleSearch = () => {
             </svg>
           </button>
         )} */}
-        {/* Microphone icon */}
-        {/* <button
+      {/* Microphone icon */}
+      {/* <button
           type="button"
           onClick={handleVoiceSearch}
           className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none microphone-button"
@@ -228,84 +234,84 @@ const handleSearch = () => {
             />
           </svg>
         </button> */}
-        {/* Submit button (hidden) */}
-        {/* <button type="submit" className="hidden"></button>
+      {/* Submit button (hidden) */}
+      {/* <button type="submit" className="hidden"></button>
       </form> */}
 
       <div className='relative'>
-  <form
-    className="relative flex items-center max-w-lg mx-auto"
-    onSubmit={(e) => {
-      e.preventDefault();
-      handleSearch();
-    }}
-  >
-    <label htmlFor="voice-search" className="sr-only">
-      Search
-    </label>
-    <input
-      type="text"
-      id="voice-search"
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="World Cup Name,International Women WorldCup Name .."
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      required
-    />
-    
-    {/* Clear button */}
-    {searchQuery && (
-      <button
-        type="button"
-        onClick={handleClearSearch}
-        className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-400 hover:text-gray-600 focus:outline-none cross-button"
-        style={{ top: '50%', transform: 'translateY(-50%)', marginRight: '35px' }}
-      >
-        <svg
-          className="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <form
+          className="relative flex items-center max-w-lg mx-auto"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
         >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-        </svg>
-      </button>
-    )}
-    {/* Microphone icon */}
-    <button
-      type="button"
-      onClick={handleVoiceSearch}
-      className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none microphone-button"
-      style={{ top: '50%', transform: 'translateY(-50%)', marginRight: '5px' }}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-      >
-        <path fill="none" d="M0 0h24v24H0z" />
-        <path
-          fill="#4285F4"
-          d="M12 15c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v7c0 1.66 1.34 3 3 3z"
-        />
-        <path fill="#34A853" d="M11 18.92h2V22h-2z" />
-        <path
-          fill="#F4B400"
-          d="M7 12H5c0 1.93.78 3.68 2.05 4.95l1.41-1.41C7.56 14.63 7 13.38 7 12z"
-        />
-        <path
-          fill="#EA4335"
-          d="M12 17c-1.38 0-2.63-.56-3.54-1.47l-1.41 1.41A6.99 6.99 0 0 0 12.01 19c3.87 0 6.98-3.14 6.98-7h-2c0 2.76-2.23 5-4.99 5z"
-        />
-      </svg>
-    </button>
-    {/* Submit button (hidden) */}
-    <button type="submit" className="hidden"></button>
-  </form>
-</div>
+          <label htmlFor="voice-search" className="sr-only">
+            Search
+          </label>
+          <input
+            type="text"
+            id="voice-search"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="World Cup Name,International Women WorldCup Name .."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            required
+          />
+
+          {/* Clear button */}
+          {searchQuery && (
+            <button
+              type="button"
+              onClick={handleClearSearch}
+              className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-400 hover:text-gray-600 focus:outline-none cross-button"
+              style={{ top: '50%', transform: 'translateY(-50%)', marginRight: '35px' }}
+            >
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          )}
+          {/* Microphone icon */}
+          <button
+            type="button"
+            onClick={handleVoiceSearch}
+            className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none microphone-button"
+            style={{ top: '50%', transform: 'translateY(-50%)', marginRight: '5px' }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                fill="#4285F4"
+                d="M12 15c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v7c0 1.66 1.34 3 3 3z"
+              />
+              <path fill="#34A853" d="M11 18.92h2V22h-2z" />
+              <path
+                fill="#F4B400"
+                d="M7 12H5c0 1.93.78 3.68 2.05 4.95l1.41-1.41C7.56 14.63 7 13.38 7 12z"
+              />
+              <path
+                fill="#EA4335"
+                d="M12 17c-1.38 0-2.63-.56-3.54-1.47l-1.41 1.41A6.99 6.99 0 0 0 12.01 19c3.87 0 6.98-3.14 6.98-7h-2c0 2.76-2.23 5-4.99 5z"
+              />
+            </svg>
+          </button>
+          {/* Submit button (hidden) */}
+          <button type="submit" className="hidden"></button>
+        </form>
+      </div>
 
       {isDanger && (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mt-3 rounded relative" role="alert">
@@ -313,7 +319,7 @@ const handleSearch = () => {
           <span className="block sm:inline"> Please try a different search query.</span>
         </div>
       )}
-      {isSuccess &&(
+      {isSuccess && (
         <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mt-4 rounded relative" role="alert">
           <strong className="font-bold">Great!</strong>
           <span className="block sm:inline"> Showing the result for {searchQuery}</span>
@@ -338,38 +344,38 @@ const handleSearch = () => {
 
 
       {isSearchBox && (
-  <div className='searchbox absolute z-50 mt-2 w-full'>
-    <ul className="bg-white border border-gray-200 rounded-lg w-auto shadow-md sm:w-full lg:w-[80%] md:w-[80%]">
-      {filteredCups.map((cup) => (
-        <li
-          key={cup.league.id}
-          className="px-4 py-2 cursor-pointer hover:bg-gray-100 flex items-center"
-        >
-          <Link to={`/cup/${cup.league.id}`} onClick={() => setSearchBox(false)}>
-            <div className="flex items-center"> {/* Wrap the elements in a flex container */}
-              <svg
-                width="20"
-                height="20"
-                className="DocSearch-Search-Icon mr-2"
-                viewBox="0 0 20 20"
+        <div className='searchbox absolute z-50 mt-2 w-full'>
+          <ul className="bg-white border border-gray-200 rounded-lg w-auto shadow-md sm:w-full lg:w-[80%] md:w-[80%]">
+            {filteredCups.map((cup) => (
+              <li
+                key={cup.league.id}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100 flex items-center"
               >
-                <path
-                  d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
-                  stroke="currentColor"
-                  fill="none"
-                  fill-rule="evenodd"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <span>{cup.league.name}</span> {/* Cup name */}
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-)}
+                <Link to={`/cup/${cup.league.id}`} onClick={() => setSearchBox(false)}>
+                  <div className="flex items-center"> {/* Wrap the elements in a flex container */}
+                    <svg
+                      width="20"
+                      height="20"
+                      className="DocSearch-Search-Icon mr-2"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                        stroke="currentColor"
+                        fill="none"
+                        fill-rule="evenodd"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                    <span>{cup.league.name}</span> {/* Cup name */}
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
 
 
