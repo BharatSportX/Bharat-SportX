9b634a2673f88b8656dd602efd85fc2c76339cf1
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-05 00:45:18 +0530

    update navbar

--- client/src/components/Home/Navbar/Navbar.js
+++ client/src/components/Home/Navbar/Navbar.js
@@ -178,10 +178,217 @@
 // };
 
 // export default Navbar;
+// import React, { useState, useRef, useEffect } from "react";
+// import "./Navbar.css";
+// import { NavLink } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// const Navbar = () => {
+//   const [openMenu, setOpenMenu] = useState(false);
+//   const menuRef = useRef(null);
+
+//   useEffect(() => {
+//     const handleOutsideClick = (event) => {
+//       if (menuRef.current && !menuRef.current.contains(event.target)) {
+//         setOpenMenu(false);
+//       }
+//     };
+
+//     document.addEventListener("mousedown", handleOutsideClick);
+
+//     return () => {
+//       document.removeEventListener("mousedown", handleOutsideClick);
+//     };
+//   }, []);
+
+//   const handleOpenMenu = () => {
+//     setOpenMenu(true);
+//   };
+
+//   return (
+//     <nav className="bg-gray-800">
+//       <div className="px-4 md:px-10 xl:px-12">
+//         <div className="flex items-center justify-between h-16">
+//           {/* Logo and Company Name */}
+//           <div className="text-white space-x-3 flex items-center xl:space-x-6">
+//             <img
+//               className="h-8 w-auto cursor-pointer"
+//               src="sportlogo.svg"
+//               alt="Logo"
+//             />
+//             <form>
+//               <button
+//                 className="w-28 xl:w-24 px-0 font-semibold text-xs BtnEx1 z-10"
+//                 style={{ height: "31px" }}
+//               >
+//                 SUBSCRIBE
+//               </button>
+//             </form>
+//             {/* Navigation */}
+//             {/* Navigation menu */}
+//             <div className="relative" ref={menuRef}>
+//               <button
+//                 className="focus:outline-none text-white xl:hidden"
+//                 onClick={handleOpenMenu}
+//               >
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   className="h-6 w-6"
+//                   fill="none"
+//                   viewBox="0 0 24 24"
+//                   stroke="currentColor"
+//                 >
+//                   <path
+//                     strokeLinecap="round"
+//                     strokeLinejoin="round"
+//                     strokeWidth={2}
+//                     d="M4 6h16M4 12h16M4 18h16"
+//                   />
+//                 </svg>
+//               </button>
+//               {openMenu && (
+//                 <div className="absolute top-14 right-0 bg-gray-800 border border-gray-700 rounded-md p-2">
+//                   <NavLink
+//                     to="#"
+//                     className="block text-white py-2 px-4 hover:bg-gray-700"
+//                   >
+//                     Home
+//                   </NavLink>
+//                   <NavLink
+//                     to="#"
+//                     className="block text-white py-2 px-4 hover:bg-gray-700"
+//                   >
+//                     About
+//                   </NavLink>
+//                   <NavLink
+//                     to="#"
+//                     className="block text-white py-2 px-4 hover:bg-gray-700"
+//                   >
+//                     Services
+//                   </NavLink>
+//                   <NavLink
+//                     to="#"
+//                     className="block text-white py-2 px-4 hover:bg-gray-700"
+//                   >
+//                     Contact
+//                   </NavLink>
+//                 </div>
+//               )}
+//             </div>
+//             <Link
+//               href="#"
+//               className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+//             >
+//               <div className="paste-button">
+//                 <button className="button45  font-semibold text-gray-300 text-[1.1rem]">
+//                   More{" "}
+//                   <span>
+//                     <svg
+//                       width="15"
+//                       height="15"
+//                       viewBox="0 0 15 15"
+//                       fill="none"
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       className="size-[20px] stroke-white stroke-[1px] animate-paste-arrow  opacity-70  text-white"
+//                       aria-hidden="true"
+//                     >
+//                       <path
+//                         d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+//                         fill="currentColor"
+//                         fillRule="evenodd"
+//                         clipRule="evenodd"
+//                       ></path>
+//                     </svg>
+//                   </span>
+//                 </button>
+//                 <div className="dropdown-content">
+//                   <Link id="top" href="#">
+//                     Keep source formatting
+//                   </Link>
+//                   <Link id="middle" href="#">
+//                     Merge formatting
+//                   </Link>
+//                   <Link id="bottom" href="#">
+//                     Keep text only
+//                   </Link>
+//                 </div>
+//               </div>
+//             </Link>
+//           </div>
+//           {/* Search bar */}
+//           <div className="hidden flex-1 xl:flex justify-center xl:justify-end">
+//             <div className="relative">
+//               <input
+//                 type="text"
+//                 placeholder="Search Here..."
+//                 className="bg-gray-700 text-white rounded-full pl-12 w-80 pr-14 py-1 focus:outline-none focus:bg-gray-900"
+//                 onClick={handleOpenMenu}
+//               />
+//               <button className="absolute px-1 left-2 top-0 mt-[0.4rem] mr-2">
+//                 <svg
+//                   className="size-5 text-gray-400"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   viewBox="0 0 24 24"
+//                   fill="none"
+//                   stroke="currentColor"
+//                   strokeWidth="2"
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                 >
+//                   <circle cx="11" cy="11" r="8"></circle>
+//                   <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+//                 </svg>
+//               </button>
+//               <button className="absolute hover:rounded-full hover:bg-slate-300 size-7 items-center flex justify-center right-2 top-0 mt-1 mr-1">
+//                 <svg
+//                   className="h-5 w-5 text-gray-400 hover:text-gray-700"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   fill="currentColor"
+//                   viewBox="0 0 16 19"
+//                 >
+//                   <path d="M15 5a1 1 0 0 0-1 1v3a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V6a1 1 0 0 0-2 0v3a6.006 6.006 0 0 0 6 6h1v2H5a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2H9v-2h1a6.006 6.006 0 0 0 6-6V6a1 1 0 0 0-1-1Z" />
+//                   <path d="M9 0H7a3 3 0 0 0-3 3v5a3 3 0 0 0 3 3h2a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3Z" />
+//                 </svg>
+//               </button>
+//             </div>
+//           </div>
+//           <div className="flex xl:items-center xl:ml-4">
+//             <button className="hover:rounded-full mx-4 my-0 size-8 hover:border hover:border-gray-700 hover:focus:ring-slate-900 hover:focus:ring-4 items-center flex justify-center hover:bg-slate-200 hover:bg-opacity-5">
+//               <svg
+//                 className="size-6 text-white xl:hidden"
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 viewBox="0 0 24 24"
+//                 fill="none"
+//                 stroke="currentColor"
+//                 strokeWidth="2"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//               >
+//                 <circle cx="11" cy="11" r="8"></circle>
+//                 <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+//               </svg>
+//             </button>
+//             {/* User profile */}
+//             <button className="hover:rounded-full size-8 hover:border hover:border-gray-700 hover:focus:ring-slate-900 hover:focus:ring-4 items-center flex justify-center hover:bg-slate-200 hover:bg-opacity-5">
+//               <img
+//                 className="h-8 w-8 rounded-full"
+//                 src="https://via.placeholder.com/30"
+//                 alt="User"
+//               />
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React, { useState, useRef, useEffect } from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuRef = useRef(null);
@@ -217,7 +424,7 @@ const Navbar = () => {
             />
             <form>
               <button
-                className="w-28 xl:w-24 px-0 font-semibold text-xs BtnEx1 z-10"
+                className="w-28 xl:w-24 px-0 font-semibold text-xs BtnEx1 z-10 hidden xl:block"
                 style={{ height: "31px" }}
               >
                 SUBSCRIBE
@@ -263,7 +470,7 @@ const Navbar = () => {
                     to="#"
                     className="block text-white py-2 px-4 hover:bg-gray-700"
                   >
-                    Services
+                    <button className="w-full">Subscribe</button>
                   </NavLink>
                   <NavLink
                     to="#"
