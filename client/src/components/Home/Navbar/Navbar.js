c677a74925aec362624f32b0a201ef2b7db98b5a
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-05 19:46:33 +0530

    special

--- client/src/components/Home/Navbar/Navbar.js
+++ client/src/components/Home/Navbar/Navbar.js
@@ -10,8 +10,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800">
-      <div className="px-4 md:px-10 xl:px-12">
+    <nav className="dark:bg-gray-800 bg-slate-300">
+      <div className="px-4 md:px-14  lg:px-16 xl:px-12">
         <div className="flex items-center justify-between h-16">
           {/* Logo and Company Name */}
           <div className="text-white space-x-3 flex items-center xl:space-x-6">
@@ -32,34 +32,34 @@ const Navbar = () => {
             {/* Navigation */}
             <NavLink
               to="#"
-              className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+              className="hidden xl:block xl:font-semibold xl:text-gray-900 xl:dark:text-gray-300 xl:text-[1.1rem]"
             >
               Home
             </NavLink>
             <NavLink
               to="#"
-              className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+              className="hidden xl:block xl:font-semibold xl:text-gray-900 xl:dark:text-gray-300 xl:text-[1.1rem]"
             >
               About
             </NavLink>
             <NavLink
               to="#"
-              className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+              className="hidden xl:block xl:font-semibold xl:text-gray-900 xl:dark:text-gray-300 xl:text-[1.1rem]"
             >
               Services
             </NavLink>
             <NavLink
               to="#"
-              className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+              className="hidden xl:block xl:font-semibold xl:text-gray-900 xl:dark:text-gray-300 xl:text-[1.1rem]"
             >
               Contact
             </NavLink>
             <NavLink
               to="#"
-              className="hidden xl:block xl:font-semibold xl:text-gray-300 xl:text-[1.1rem]"
+              className="hidden xl:block xl:font-semibold xl:text-gray-900 xl:dark:text-gray-300 xl:text-[1.1rem]"
             >
               <div className="paste-button">
-                <button className="button45 font-semibold text-gray-300 text-[1.1rem]">
+                <button className="button45 font-semibold  text-[1.1rem]">
                   More{" "}
                   <span>
                     <svg
@@ -135,12 +135,12 @@ const Navbar = () => {
           {/* Mobile Search button */}
           <div className="flex xl:items-center xl:ml-4">
             <button
-              className="hover:rounded-full mx-4 my-0 size-8 hover:border hover:border-gray-700 hover:focus:ring-slate-900 hover:focus:ring-4 items-center flex justify-center xl:hidden hover:bg-slate-200 hover:bg-opacity-5"
+              className="hover:rounded-full mx-4 my-0 size-8 hover:border hover:border-gray-700 hover:focus:ring-slate-900 hover:focus:ring-4 items-center flex justify-center xl:hidden  hover:bg-opacity-5 "
               title="Search"
               onClick={toggleSearch}
             >
               <svg
-                className="size-6 text-white xl:hidden"
+                className="size-6 dark:text-white text-gray-800 xl:hidden"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="none"
@@ -209,16 +209,16 @@ const Navbar = () => {
         </div>
       </div>
 
-{/* Mobile navbar */}
+      {/* Mobile navbar */}
 
       <div className=" xl:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-800 dark:border-gray-600">
-        <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+        <div className="grid h-full max-w-lg grid-cols-5 m-auto  font-medium md:flex md:justify-center">
           <button
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 md:px-14  lg:px-16 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
-              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
+              className="size-4 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -226,16 +226,16 @@ const Navbar = () => {
             >
               <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
+            <span className="text-xs text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
               Home
             </span>
           </button>
           <button
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 md:px-14  lg:px-16 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
-              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
+              className="size-4 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -244,16 +244,33 @@ const Navbar = () => {
               <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z" />
               <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z" />
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
+            <span className="text-xs text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
               Wallet
             </span>
           </button>
           <button
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 md:px-14  lg:px-16 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
-              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
+              xmlns="http://www.w3.org/2000/svg"
+              className=" mb-2  text-gray-500 dark:text-gray-400
+            group-hover:text-red-600 dark:group-hover:text-red-500"
+            width="44" height="44"
+              fill="gray"
+              class="bi bi-arrow-up-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+            </svg>
+            
+          </button>
+          <button
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 md:px-14  lg:px-16 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              className="size-4 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -267,16 +284,16 @@ const Navbar = () => {
                 d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
               />
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
+            <span className="text-xs text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
               Settings
             </span>
           </button>
           <button
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 md:px-14  lg:px-16 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <svg
-              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
+              className="size-4 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -284,7 +301,7 @@ const Navbar = () => {
             >
               <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
+            <span className="text-xs text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
               Profile
             </span>
           </button>
