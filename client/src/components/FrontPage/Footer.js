6cf25c6351f54bb70d1f899ac2e5658d7c0a3e15
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-05 11:22:45 +0530

    foter modified

--- client/src/components/FrontPage/Footer.js
+++ client/src/components/FrontPage/Footer.js
@@ -1,73 +1,218 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// const Footer = () => {
+//   return (
+//     <div>
+//       <footer class="p-4 md:p-8 lg:p-10 bg-black mt-2">
+//         <div class="mx-auto max-w-screen-xl text-center">
+//         <a
+//             href="/"
+//             // className="flex items-center space-x-3 rtl:space-x-reverse"
+//           >
+//             <span className="self-center text-3xl  font-semibold whitespace-nowrap  font-[Charm,cursive] pl-3 md:text-[2.5rem]">
+//               <span className="text-orange-700 text-4xl md:text-[2.9rem]">B</span>
+//               <span className="text-orange-700">h</span>
+//               <span className=" text-white">ar</span>
+//               <span className=" text-green-600">at</span>
+//               <span className=" text-[#ffb300]">SportX</span>
+//             </span>
+//           </a>
+//           <p class="my-6 text-gray-400">
+//             Open-source library of over 400+ web components and interactive
+//             elements built for better web.
+//           </p>
+//           <ul class="flex flex-wrap justify-center items-center mb-6 text-white">
+//             <li>
+//               <Link href="#" class="mr-4 hover:underline md:mr-6 ">
+//                 About
+//               </Link>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6">
+//                 Premium
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6 ">
+//                 Campaigns
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6">
+//                 Blog
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6">
+//                 Affiliate Program
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6">
+//                 FAQs
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#" class="mr-4 hover:underline md:mr-6">
+//                 Contact
+//               </a>
+//             </li>
+//           </ul>
+//           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+//             © 2021-2022{" "}
+//             <a href="#" class="hover:underline">
+//               BharatSportX™
+//             </a>
+//             . All Rights Reserved.
+//           </span>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+// import React from "react";
+
+// const Footer = () => {
+//   // Get current year and next year
+//   const currentYear = new Date().getFullYear();
+//   const nextYear = currentYear + 1;
+
+//   return (
+//     <footer className="p-4 md:p-8 lg:p-10 bg-black mt-2">
+//       <div className="mx-auto max-w-screen-xl text-center">
+//         <a href="/">
+//           <span className="self-center text-3xl font-semibold whitespace-nowrap font-[Charm,cursive] pl-3 md:text-[2.5rem]">
+//             <span className="text-orange-700 text-4xl md:text-[2.9rem]">B</span>
+//             <span className="text-orange-700">h</span>
+//             <span className="text-white">ar</span>
+//             <span className="text-green-600">at</span>
+//             <span className="text-[#ffb300]">SportX</span>
+//           </span>
+//         </a>
+//         <p className="my-6 text-gray-400">
+//           Open-source library of over 400+ web components and interactive elements built for better web.
+//         </p>
+//         <ul className="flex flex-wrap justify-center items-center mb-6 text-white">
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               About
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               Premium
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               Campaigns
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               Blog
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               Affiliate Program
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               FAQs
+//             </a>
+//           </li>
+//           <li>
+//             <a href="#" className="mr-4 hover:underline md:mr-6">
+//               Contact
+//             </a>
+//           </li>
+//         </ul>
+//         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+//           © {currentYear}-{nextYear}{" "}
+//           <a href="#" className="hover:underline">
+//             BharatSportX™
+//           </a>
+//           . All Rights Reserved.
+//         </span>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  // Get current year and next year
+  const currentYear = new Date().getFullYear();
+  const nextYear = currentYear + 1;
+
   return (
-    <div>
-      <footer class="p-4 md:p-8 lg:p-10 bg-black mt-2">
-        <div class="mx-auto max-w-screen-xl text-center">
-        <a
-            href="/"
-            // className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <span className="self-center text-3xl  font-semibold whitespace-nowrap  font-[Charm,cursive] pl-3 md:text-[2.5rem]">
-              <span className="text-orange-700 text-4xl md:text-[2.9rem]">B</span>
-              <span className="text-orange-700">h</span>
-              <span className=" text-white">ar</span>
-              <span className=" text-green-600">at</span>
-              <span className=" text-[#ffb300]">SportX</span>
-            </span>
-          </a>
-          <p class="my-6 text-gray-400">
-            Open-source library of over 400+ web components and interactive
-            elements built for better web.
-          </p>
-          <ul class="flex flex-wrap justify-center items-center mb-6 text-white">
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Premium
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
-                Campaigns
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Affiliate Program
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                FAQs
-              </a>
-            </li>
-            <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2021-2022{" "}
-            <a href="#" class="hover:underline">
-              BharatSportX™
-            </a>
-            . All Rights Reserved.
+    <footer className="p-4 md:p-8 lg:p-10 bg-black mt-2">
+      <div className="mx-auto max-w-screen-xl text-center">
+        <Link to="/">
+          <span className="self-center text-3xl font-semibold whitespace-nowrap font-[Charm,cursive] pl-3 md:text-[2.5rem]">
+            <span className="text-orange-700 text-4xl md:text-[2.9rem]">B</span>
+            <span className="text-orange-700">h</span>
+            <span className="text-white">ar</span>
+            <span className="text-green-600">at</span>
+            <span className="text-[#ffb300]">SportX</span>
           </span>
-        </div>
-      </footer>
-    </div>
+        </Link>
+        <p className="my-6 text-gray-400">
+          Your go-to platform for all things sports-related, offering live scores, player details, match predictions, statistics, and much more!
+        </p>
+        <ul className="flex flex-wrap justify-center items-center mb-6 text-white">
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              About BharatSportX
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              Premium Subscription
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              Campaigns
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              Affiliate Program
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              FAQs
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="mr-4 hover:underline md:mr-6">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {currentYear}-{nextYear}{" "}
+          <span className="hover:underline">
+            BharatSportX™
+          </span>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
   );
 };
 
--- client/src/components/FrontPage/FrontBody/FrontBody.js
+++ client/src/components/FrontPage/FrontBody/FrontBody.js
@@ -194,7 +194,7 @@ const FrontBody = () => {
           isOpen={openAccordionIndex === 7}
           toggleAccordion={() => toggleAccordion(7)}
         />
-         <BodyQN
+        <BodyQN
           title="Is there a mobile app available for your website?"
           answer="No, we are currently focused on delivering the best experience through our web version and mobile website. However, stay tuned as we are working on launching mobile apps for both iOS and Android platforms in the near future, providing you with access to live scores, player statistics, and match predictions on the go."
           isOpen={openAccordionIndex === 8}
@@ -202,87 +202,87 @@ const FrontBody = () => {
         />
         {/* Subscribe Features Accordion Item */}
         <h1 className="text-3xl p-11 leading-snug lg:text-5xl text-white text-center pt-12 pb-6 font-[Rowdies,sans-serif] lg:font-bold">
-  Subscribe Features{" "}
-</h1>
+          Subscribe Features{" "}
+        </h1>
 
-<BodyQN
-  title="What additional features are included in the subscription to BharatSportX?"
-  answer={
-    <div>
-      <p className="mb-4 font-bold">Subscribing to BharatSportX unlocks a range of additional features designed to enhance your sports experience. Some of these features include:</p>
-      <ul className="list-disc pl-6 mb-6">
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Advanced statistics:</span>
-          </span>
-          <span className="ml-8">Access in-depth player and team statistics, including historical data and performance trends.</span>
-        </li>
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Enhanced prediction tools:</span>
-          </span>
-          <span className="ml-8">Gain access to advanced prediction algorithms and insights, improving the accuracy of your match predictions.</span>
-        </li>
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Player details with prediction statistics:</span>
-          </span>
-          <span className="ml-8">Explore detailed player profiles with integrated prediction statistics, allowing you to make informed decisions based on player performance and potential outcomes.</span>
-        </li>
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Exclusive content:</span>
-          </span>
-          <span className="ml-8">Enjoy exclusive articles, interviews, and analysis from renowned sports experts, providing valuable insights and perspectives.</span>
-        </li>
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Personalized experience:</span>
-          </span>
-          <span className="ml-8">Customize your dashboard and notifications to receive updates tailored to your favorite teams, players, and leagues.</span>
-        </li>
-        <li className="mb-2 flex flex-col">
-          <span className="flex items-center">
-            <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="font-bold">Priority customer support:</span>
-          </span>
-          <span className="ml-8">Receive priority assistance from our dedicated support team, ensuring that your questions and concerns are addressed promptly and effectively.</span>
-        </li>
-      </ul>
-    </div>
-  }
-  isOpen={openAccordionIndex === 9}
-  toggleAccordion={() => toggleAccordion(9)}
-/>
-
-
+        <BodyQN
+          title="What additional features are included in the subscription to BharatSportX?"
+          answer={
+            <div>
+              <p className="mb-4 font-bold">Subscribing to BharatSportX unlocks a range of additional features designed to enhance your sports experience. Some of these features include:</p>
+              <ul className="list-disc pl-6 mb-6">
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Advanced statistics:</span>
+                  </span>
+                  <span className="ml-8">Access in-depth player and team statistics, including historical data and performance trends.</span>
+                </li>
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Enhanced prediction tools:</span>
+                  </span>
+                  <span className="ml-8">Gain access to advanced prediction algorithms and insights, improving the accuracy of your match predictions.</span>
+                </li>
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Player details with prediction statistics:</span>
+                  </span>
+                  <span className="ml-8">Explore detailed player profiles with integrated prediction statistics, allowing you to make informed decisions based on player performance and potential outcomes.</span>
+                </li>
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Exclusive content:</span>
+                  </span>
+                  <span className="ml-8">Enjoy exclusive articles, interviews, and analysis from renowned sports experts, providing valuable insights and perspectives.</span>
+                </li>
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Personalized experience:</span>
+                  </span>
+                  <span className="ml-8">Customize your dashboard and notifications to receive updates tailored to your favorite teams, players, and leagues.</span>
+                </li>
+                <li className="mb-2 flex flex-col">
+                  <span className="flex items-center">
+                    <svg className="h-5 w-5 mr-2 mt-1 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="font-bold">Priority customer support:</span>
+                  </span>
+                  <span className="ml-8">Receive priority assistance from our dedicated support team, ensuring that your questions and concerns are addressed promptly and effectively.</span>
+                </li>
+              </ul>
+            </div>
+          }
+          isOpen={openAccordionIndex === 9}
+          toggleAccordion={() => toggleAccordion(9)}
+        />
 
 
-<BodyQN
-  title="What are the benefits of subscribing to BharatSportX?"
-  answer="Subscribing to BharatSportX unlocks a range of benefits, including access to exclusive content, advanced features such as personalized notifications and alerts, ad-free browsing experience, and priority customer support."
-  isOpen={openAccordionIndex === 10}
-  toggleAccordion={() => toggleAccordion(10)}
-/>
 
-<BodyQN
-  title="Is there a trial period available for subscriptions?"
-  answer="Yes, BharatSportX offers a trial period for new subscribers. During this period, you can explore the platform and its features before making a commitment. The duration of the trial period may vary, so be sure to check the current offer."
-  isOpen={openAccordionIndex === 11}
-  toggleAccordion={() => toggleAccordion(11)}
-/>
 
-<BodyQN
-  title="Can I cancel my subscription at any time?"
-  answer="Yes, you can cancel your subscription to BharatSportX at any time. There are no long-term contracts or commitments, and you can manage your subscription settings directly from your account dashboard."
-  isOpen={openAccordionIndex === 12}
-  toggleAccordion={() => toggleAccordion(12)}
-/>
+        <BodyQN
+          title="What are the benefits of subscribing to BharatSportX?"
+          answer="Subscribing to BharatSportX unlocks a range of benefits, including access to exclusive content, advanced features such as personalized notifications and alerts, ad-free browsing experience, and priority customer support."
+          isOpen={openAccordionIndex === 10}
+          toggleAccordion={() => toggleAccordion(10)}
+        />
+
+        <BodyQN
+          title="Is there a trial period available for subscriptions?"
+          answer="Yes, BharatSportX offers a trial period for new subscribers. During this period, you can explore the platform and its features before making a commitment. The duration of the trial period may vary, so be sure to check the current offer."
+          isOpen={openAccordionIndex === 11}
+          toggleAccordion={() => toggleAccordion(11)}
+        />
+
+        <BodyQN
+          title="Can I cancel my subscription at any time?"
+          answer="Yes, you can cancel your subscription to BharatSportX at any time. There are no long-term contracts or commitments, and you can manage your subscription settings directly from your account dashboard."
+          isOpen={openAccordionIndex === 12}
+          toggleAccordion={() => toggleAccordion(12)}
+        />
 
 
         {/* Subscription Button */}
