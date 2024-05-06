a6c144ff4391f181744086e44d5f3d42aeb222bb
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-06 09:18:33 +0530

    updated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,20 +1,13 @@
-import React from "react";
-import Navbar from "./Navbar/Navbar";
-import { useLocation } from "react-router-dom";
-import HomeNav from "./HomeNav";
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import HomeNav from './HomeNav'
 
-const Home = () => {
-  const location = useLocation();
-
+export default function Home() {
   return (
     <div>
-      <div className="relative z-[0] h-screen bg-slate-200">
-        <Navbar />
-      </div>
-      {/* Navbar code should be placed inside the div containing the relative positioning */}
-      <HomeNav location={location} />
+      <Navbar/>
+      <HomeNav/>
     </div>
-  );
-};
-
-export default Home;
+    
+  )
+}
