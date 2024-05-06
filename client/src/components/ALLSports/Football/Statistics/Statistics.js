bf2c0c30d46c20718050a708fc65e3b66747d167
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-06 15:08:37 +0530

    stat updated will updating soon

--- client/src/components/ALLSports/Football/Statistics/Statistics.js
+++ client/src/components/ALLSports/Football/Statistics/Statistics.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Statistics() {
+  const [StatData, SetStatData] = useState(null)
+  const { id } = useParams()
+  const {fixture}=useParams()
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      const response = await axios.get("https://api-football-v1.p.rapidapi.com/v3/fixtures/statistics", {
+        params: {
+          fixture: fixture,
+          id: id
+        }
+
+      }
+
+      )
+      SetStatData(response.response[0].data)
+    }
+    fetchStatistics()
+  }, [id, fixture])
   return (
-    <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-      <strong className="font-bold">Added Statistics Soon...</strong>
-      <span className="block sm:inline"> Stay Tuned</span>
+    <div className='container '>
+
+      <div className="bg-red-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mt-3" role="alert">
+        <strong className="font-bold">Added Statistics Soon...</strong>
+        <span className="block sm:inline"> Stay Tuned</span>
+      </div>
+      <div>
+        <h1> {StatData.statistics.type.value}</h1>
+      </div>
+
     </div>
   );
 }
