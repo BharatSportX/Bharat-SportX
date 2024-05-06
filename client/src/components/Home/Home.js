7849f8792b8cb7cbb8f12ef28944ecb5fe49fca3
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-06 09:49:22 +0530

    updated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import HomeNav from './HomeNav'
+
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <HomeNav/>
+   
     </div>
     
   )
