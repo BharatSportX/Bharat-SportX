9597c14d0af2f135bf9ce48466cd243b616dc92e
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-11 14:37:59 +0530

    Updated stats pages

--- client/src/components/ALLSports/Football/Statistics/Statistics.js
+++ client/src/components/ALLSports/Football/Statistics/Statistics.js
@@ -1224,7 +1224,17 @@ if (!teamStats || teamStats.length === 0) {
         <span className="icon">
           <FontAwesomeIcon icon={faChartLine} />
         </span>
+       
       </h1>
+      <div className='text-center mb-6'>
+      <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 " onClick={()=>{
+        alert("We are working on it... Please wait for few days! Thank You for your Patient!!!");
+      }}>
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        See in Table Format
+        </span>
+      </button>
+    </div>
       <div className='chart-container border border-gray-200 rounded-lg shadow-md overflow-hidden mb-6'>
         <CanvasJSChart options={options} />
       </div>
