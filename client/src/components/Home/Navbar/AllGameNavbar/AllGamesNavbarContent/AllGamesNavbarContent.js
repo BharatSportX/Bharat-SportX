232b0c1db94ee1c1353c6b7ebbfcd73eb1e2ecf4
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-17 01:02:54 +0530

    updated mt-1

--- client/src/components/Home/Navbar/AllGameNavbar/AllGamesNavbarContent/AllGamesNavbarContent.js
+++ client/src/components/Home/Navbar/AllGameNavbar/AllGamesNavbarContent/AllGamesNavbarContent.js
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function AllGamesNavbarContent() {
   return (
-    <Disclosure as="nav" className="bg-green-400 mt-2">
+    <Disclosure as="nav" className="bg-green-400 mt-1">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
