fc826e25b737cab64f27a6d8af64cf0a36e79df7
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-18 09:20:37 +0530

    Updated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import Navbar from './Navbar/Navbar'
-
-import Footer from '../FrontPage/Footer'
-
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+import Footer from '../FrontPage/Footer';
 
 export default function Home() {
   return (
     <div>
-      <React.Fragment>
-    
-      
-      <Navbar/>
-      {/* <LiveUpdateNav/>
-      <LiveUpdatesContent/> */}
-     <Footer/>
-      </React.Fragment>
+      <Navbar />
+      {/* <LiveUpdateNav />
+      <LiveUpdatesContent /> */}
+      <div>
+        <h1 className='text-center text-red-600 text-4xl md:text-6xl font-bold shadow-lg'>
+          Bara Boka choda thik kore Kaj korbi
+        </h1>
+      </div>
+      <Footer />
     </div>
-    
-  )
+  );
 }
