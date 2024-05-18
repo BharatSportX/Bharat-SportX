2b744b059d496d3308ef01a64fc8f455ed411588
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-18 15:53:57 +0530

    drawee

--- client/src/components/Home/Navbar/NavDrawer.js
+++ client/src/components/Home/Navbar/NavDrawer.js
@@ -1,27 +1,110 @@
 import React, { useState } from "react";
 
 const NavDrawer = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
+    setIsOpen(!isOpen);
   };
 
   return (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className=" mb-2  text-gray-500 dark:text-gray-400
+    <div>
+      {/* Button to toggle drawer */}
+      <div className="text-center">
+        <button onClick={toggleDrawer} className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className=" mb-2  text-gray-500 dark:text-gray-400
             group-hover:text-red-600 dark:group-hover:text-red-500"
-        width="44"
-        height="44"
-        fill="gray"
-        class="bi bi-arrow-up-circle-fill"
-        viewBox="0 0 16 16"
+            width="44"
+            height="44"
+            fill="gray"
+            class="bi bi-arrow-up-circle-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+          </svg>
+        </button>
+      </div>
+
+      {/* Drawer component */}
+      <div
+        className={`fixed z-40 w-full overflow-y-auto bg-white border-t border-gray-200 rounded-t-lg dark:border-gray-700 dark:bg-gray-800 transition-transform transform ${
+          isOpen ? "translate-y-0" : "translate-y-full"
+        } bottom-0 left-0 right-0 xl:hidden`}
+        tabindex="-1"
       >
-        <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
-      </svg>
-    </>
+        <div
+          className="p-4 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-700"
+          onClick={toggleDrawer}
+        >
+          <span className="absolute w-8 h-1 -translate-x-1/2 bg-gray-300 rounded-lg top-3 left-1/2 dark:bg-gray-600"></span>
+          <h5 className="inline-flex items-center text-base text-gray-500 dark:text-gray-400 font-medium">
+            <svg
+              className="w-4 h-4 mr-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 18"
+            >
+              <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10ZM17 13h-2v-2a1 1 0 0 0-2 0v2h-2a1 1 0 0 0 0 2h2v2a1 1 0 0 0 2 0v-2h2a1 1 0 0 0 0-2Z" />
+            </svg>
+            Add widget
+          </h5>
+        </div>
+        <div className="grid grid-cols-3 gap-4 p-4 xl:grid-cols-4">
+          <div className="p-4 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700">
+            <div className="flex justify-center items-center p-2 mx-auto mb-2 bg-gray-200 dark:bg-gray-600 rounded-full w-[48px] h-[48px] max-w-[48px] max-h-[48px]">
+              <svg
+                className="inline w-5 h-5 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 21"
+              >
+                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+              </svg>
+            </div>
+            <div className="font-medium text-center text-gray-500 dark:text-gray-400">
+              Chart
+            </div>
+          </div>
+          <div className="p-4 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700">
+            <div className="flex justify-center items-center p-2 mx-auto mb-2 bg-gray-200 dark:bg-gray-600 rounded-full w-[48px] h-[48px] max-w-[48px] max-h-[48px]">
+              <svg
+                className="inline w-5 h-5 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 14"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM9 6v2H2V6h7Zm2 0h7v2h-7V6Zm-9 4h7v2H2v-2Zm9 2v-2h7v2h-7Z" />
+              </svg>
+            </div>
+            <div className="font-medium text-center text-gray-500 dark:text-gray-400">
+              Table
+            </div>
+          </div>
+          <div className="hidden p-4 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700 xl:block">
+            <div className="flex justify-center items-center p-2 mx-auto mb-2 bg-gray-200 dark:bg-gray-600 rounded-full w-[48px] h-[48px] max-w-[48px] max-h-[48px]">
+              <svg
+                className="inline w-5 h-5 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 14 20"
+              >
+                <path d="M13.383.076a1 1 0 0 0-1.09.217L11 1.586 9.707.293a1 1 0 0 0-1.414 0L7 1.586 5.707.293a1 1 0 0 0-1.414 0L3 1.586 1.707.293A1 1 0 0 0 0 1v18a1 1 0 0 0 1.707.707L3 18.414l1.293 1.293a1 1 0 0 0 1.414 0L7 18.414l1.293 1.293a1 1 0 0 0 1.414 0L11 18.414l1.293 1.293A1 1 0 0 0 14 19V1a1 1 0 0 0-.617-.924ZM10 15H4a1 1 0 1 1 0-2h6a1 1 0 0 1 0 2Zm0-4H4a1 1 0 1 1 0-2h6a1 1 0 1 1 0 2Zm0-4H4a1 1 0 0 1 0-2h6a1 1 0 1 1 0 2Z" />
+              </svg>
+            </div>
+            <div className="hidden font-medium text-center text-gray-500 dark:text-gray-400 xl:block">
+              Form
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
