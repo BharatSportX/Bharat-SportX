7a8d879d881420229c9fd8beba397380239aea18
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-18 15:11:49 +0530

    some chagnges

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,11 +7,7 @@ export default function Home() {
     <div>
       <Navbar />
       
-      <div>
-        <h1 className='text-center text-red-600 text-4xl md:text-6xl font-bold shadow-lg'>
-          Por por Kaj kor thik kore  nale emon khisit marbo na nijer nam vule jabi
-        </h1>
-      </div>
+      
      
     </div>
   );
