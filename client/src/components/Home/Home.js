268f15e6bac9f038b362313f695a040b7d024b0b
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-18 09:23:55 +0530

    Structure modified

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      {/* <LiveUpdateNav />
-      <LiveUpdatesContent /> */}
+      
       <div>
         <h1 className='text-center text-red-600 text-4xl md:text-6xl font-bold shadow-lg'>
           Por por Kaj kor thik kore  nale emon khisit marbo na nijer nam vule jabi
