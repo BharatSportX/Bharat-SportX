c6259f50f430797fe98d71ab4842cff330295b3c
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-20 13:08:15 +0530

    done

--- client/src/components/Home/HomeBody/HomeBody.js
+++ client/src/components/Home/HomeBody/HomeBody.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
 import NavContext from '../Navbar/NavContext/NavContext'
 const HomeBody = () => {
-  const {state}=useContext(NavContext)
+  
   return (
     <div className=' overflow-x-hidden relative z-[-10] top-40'>
     {/*  for z index button may not be work think any oter way to access this search bar */}
-      {state}
+      ki boossosososod
     </div>
   )
 }
