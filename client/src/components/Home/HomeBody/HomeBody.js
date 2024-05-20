feee7e5bde5d969947c8721bc915830096f1f0b2
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-21 01:58:04 +0530

    successfull

--- client/src/components/Home/HomeBody/HomeBody.js
+++ client/src/components/Home/HomeBody/HomeBody.js
@@ -1,13 +1,29 @@
-import React, { useContext } from 'react'
-import NavContext from '../Navbar/NavContext/NavContext'
+import React, { useContext } from "react";
+import NavContext from "../Navbar/NavContext/NavContext";
+import LiveMatch from "./LiveMatch";
+import Blogs from "./Blogs";
 const HomeBody = () => {
-  
   return (
-    <div className=' overflow-x-hidden relative z-[-10] top-40'>
-    {/*  for z index button may not be work think any oter way to access this search bar */}
-      ki boossosososod
+    <div className="  relative z-[-10] top-40 m-7">
+      {/*  for z index button may not be work think any oter way to access this search bar */}
+      <div className="mx-20 mb-5 mt-3">
+        <h1 className="text-4xl font-semibold ">Matches</h1>
+      </div>
+      <div className="m-4 ml-20">
+        <span className="bg-orange-100 text-orange-800 text-xs font-medium me-3 px-4 py-1.5 rounded-full cursor-pointer dark:bg-orange-900 dark:text-orange-300">
+          Live Matches
+        </span>
+        <span className="bg-orange-100 text-orange-800 text-xs font-medium me-3 px-4 py-1.5 rounded-full cursor-pointer dark:bg-orange-900 dark:text-orange-300">
+          Recent Matches
+        </span>
+        <span className="bg-orange-100 text-orange-800 text-xs font-medium me-3 px-4 py-1.5 rounded-full cursor-pointer dark:bg-orange-900 dark:text-orange-300">
+          Upcoming Matches
+        </span>
+      </div>
+      <LiveMatch/>
+      <Blogs/>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBody
+export default HomeBody;
