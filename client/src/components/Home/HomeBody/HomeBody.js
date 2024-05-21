ad9879c8915859645d5985a564a46fb259b6b30a
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-22 02:22:10 +0530

    11

--- client/src/components/Home/HomeBody/HomeBody.js
+++ client/src/components/Home/HomeBody/HomeBody.js
@@ -15,14 +15,19 @@ const HomeBody = () => {
   return (
     <div className="relative top-36 m-4">
       <div className="">
-        <h1 className="text-3xl font-semibold " style={{fontFamily:'"Varela Round", sans-serif'}}>Matches</h1>
+        <h1
+          className="text-3xl font-semibold "
+          style={{ fontFamily: '"Varela Round", sans-serif' }}
+        >
+          Matches
+        </h1>
       </div>
       <div className="mt-5">
         <button
           className={
             activeButtons[0]
-              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5  rounded-full  cursor-pointer border-orange-700 text-orange-700"
-              : " border-2 border-orange-700  text-lg font-medium me-2.5 px-4 py-0.5  rounded-full cursor-pointer bg-orange-700 text-orange-300  "
+              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5  rounded-full  cursor-pointer border-orange-800 text-orange-800"
+              : " border-2 border-orange-800  text-lg font-medium me-2.5 px-4 py-0.5  rounded-full cursor-pointer bg-orange-800 text-orange-300  "
           }
           onClick={() => handleClick(0)}
         >
@@ -31,8 +36,8 @@ const HomeBody = () => {
         <button
           className={
             activeButtons[1]
-              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full  cursor-pointer border-orange-700 text-orange-700"
-              : " border-2 border-orange-700  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full cursor-poin9er bg-orange-700 text-orange-300"
+              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full  cursor-pointer border-orange-800 text-orange-800"
+              : " border-2 border-orange-800  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full cursor-poin9er bg-orange-800 text-orange-300"
           }
           onClick={() => handleClick(1)}
         >
@@ -41,8 +46,8 @@ const HomeBody = () => {
         <button
           className={
             activeButtons[2]
-              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full  cursor-pointer border-orange-700 text-orange-700"
-              : " border-2 border-orange-700  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full cursor-poin9er bg-orange-700 text-orange-300"
+              ? " border-2  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full  cursor-pointer border-orange-800 text-orange-800"
+              : " border-2 border-orange-800  text-lg font-medium me-2.5 px-4 py-0.5 pl-5 rounded-full cursor-poin9er bg-orange-800 text-orange-300"
           }
           onClick={() => handleClick(2)}
         >
