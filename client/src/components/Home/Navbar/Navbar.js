813795d4dfba6c8a036097ef452e9bca88ad004b
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-23 01:12:47 +0530

    updated theme

--- client/src/components/Home/Navbar/Navbar.js
+++ client/src/components/Home/Navbar/Navbar.js
@@ -131,8 +131,8 @@ const Navbar = () => {
                 </g>
               </svg>
             </NavLink>
-            <form>
-              <button className=" dark:hidden animated-button pl-4  lg:py-0.5 lg:pr-12 pr-8">
+            <form className="dark:hidden">
+              <button className="  animated-button pl-4  lg:py-0.5 lg:pr-12 pr-8">
                 <svg
                   viewBox="0 0 24 24"
                   className="arr-2 size-5  "
@@ -149,6 +149,14 @@ const Navbar = () => {
                 >
                   <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                 </svg>
+              </button>
+            </form>
+            <form className="hidden dark:inline-block">
+              <button
+                className="w-28 xl:w-24 px-0 font-semibold text-xs BtnEx1 z-10"
+                style={{ height: "31px" }}
+              >
+                SUBSCRIBE
               </button>
             </form>
 
