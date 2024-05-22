7af08499e3366b048be421a12262154e830b4bd7
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-23 01:26:31 +0530

    updated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar/Navbar';
 import HomeBody from './HomeBody/HomeBody';
+import { useLocation } from 'react-router-dom';
+
 ;
 
 export default function Home() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className=' text-black bg-gray-200 dark:text-white  dark:bg-gray-950 h-screen'>
       <Navbar />
