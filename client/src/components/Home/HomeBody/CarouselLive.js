aed3732a0e0e657c0166815b5216e07263bfc3d7
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-05-24 01:37:54 +0530

    bhjbhj

--- client/src/components/Home/HomeBody/CarouselLive.js
+++ client/src/components/Home/HomeBody/CarouselLive.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-
+import React, { useState, useRef } from "react";
 import LiveMatch from "./LiveMatch";
 
 const CarouselLive = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const items = [
     { component: <LiveMatch /> },
     { component: <LiveMatch /> },
     { component: <LiveMatch /> },
   ];
+
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
 
   const prevSlide = () => {
     setCurrentIndex((currentIndex - 1 + items.length) % items.length);
@@ -19,8 +20,32 @@ const CarouselLive = () => {
     setCurrentIndex((currentIndex + 1) % items.length);
   };
 
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.targetTouches[0].clientX;
+  };
+
+  const handleTouchMove = (e) => {
+    touchEndX.current = e.targetTouches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX.current - touchEndX.current > 50) {
+      // Swiped left
+      nextSlide();
+    }
+    if (touchStartX.current - touchEndX.current < -50) {
+      // Swiped right
+      prevSlide();
+    }
+  };
+
   return (
-    <div className="relative overflow-hidden">
+    <div
+      className="relative overflow-hidden"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div
         className="flex transition-transform duration-700 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
@@ -36,7 +61,7 @@ const CarouselLive = () => {
         onClick={prevSlide}
       >
         <span className="carousel-control-prev-icon" aria-hidden="true">
-          <i class="fas fa-caret-left"></i>
+          <i className="fas fa-caret-left"></i>
         </span>
         <span className="sr-only">Previous</span>
       </button>
@@ -45,7 +70,7 @@ const CarouselLive = () => {
         onClick={nextSlide}
       >
         <span className="carousel-control-next-icon" aria-hidden="true">
-          <i class="fas fa-caret-right"></i>
+          <i className="fas fa-caret-right"></i>
         </span>
         <span className="sr-only">Next</span>
       </button>
