68581b0f05fd4f6dca807c0adea1c6429a7471eb
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-05-28 01:36:06 +0530

    updated

--- client/src/components/ALLSports/Football/Competitions/Cups/CupPageDetails.js
+++ client/src/components/ALLSports/Football/Competitions/Cups/CupPageDetails.js
@@ -8,6 +8,9 @@ const CupPageDetails = () => {
     const [cupData, setCupData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [matchDetails, setMatchDetails] = useState(null);
+    const [seasons, setSeasons] = useState([]);
+    const [selectedYear, setSelectedYear] = useState(null);
 
     useEffect(() => {
         const fetchCupDetails = async () => {
@@ -22,7 +25,12 @@ const CupPageDetails = () => {
                         id: id
                     }
                 });
-                setCupData(res.data.response[0]);
+                const cupData = res.data.response[0];
+                setCupData(cupData);
+                setSeasons(cupData.seasons);
+                const latestSeason = cupData.seasons.sort((a, b) => b.year - a.year)[0];
+                setSelectedYear(latestSeason.year);
+                fetchMatchDetails(latestSeason.year);
                 setLoading(false);
             } catch (error) {
                 setError(error);
@@ -33,6 +41,30 @@ const CupPageDetails = () => {
         fetchCupDetails();
     }, [id]);
 
+    const fetchMatchDetails = async (season) => {
+        try {
+            const res = await axios.get(`https://api-football-v1.p.rapidapi.com/v3/fixtures`, {
+                headers: {
+                    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+                    'x-rapidapi-key': '96aaecf1damsh08a26fec449dda1p149914jsn1d8a476e9c06'
+                },
+                params: {
+                    league: id,
+                    season: season,
+                    round: 'Final'
+                }
+            });
+            setMatchDetails(res.data.response[0]);
+        } catch (error) {
+            console.error("Error fetching match details: ", error);
+        }
+    };
+
+    const handleSelectSeason = (year) => {
+        setSelectedYear(year);
+        fetchMatchDetails(year);
+    };
+  
     if (loading) {
         return (
             <div className="flex items-center justify-center w-full h-[100vh] text-gray-900 dark:text-gray-100 dark:bg-gray-950">
@@ -42,7 +74,7 @@ const CupPageDetails = () => {
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="animate-spin" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM13.6695 15.9999H10.3295L8.95053 17.8969L9.5044 19.6031C10.2897 19.8607 11.1286 20 12 20C12.8714 20 13.7103 19.8607 14.4956 19.6031L15.0485 17.8969L13.6695 15.9999ZM5.29354 10.8719L4.00222 11.8095L4 12C4 13.7297 4.54894 15.3312 5.4821 16.6397L7.39254 16.6399L8.71453 14.8199L7.68654 11.6499L5.29354 10.8719ZM18.7055 10.8719L16.3125 11.6499L15.2845 14.8199L16.6065 16.6399L18.5179 16.6397C19.4511 15.3312 20 13.7297 20 12L19.997 11.81L18.7055 10.8719ZM12 9.536L9.656 11.238L10.552 14H13.447L14.343 11.238L12 9.536ZM14.2914 4.33299L12.9995 5.27293V7.78993L15.6935 9.74693L17.9325 9.01993L18.4867 7.3168C17.467 5.90685 15.9988 4.84254 14.2914 4.33299ZM9.70757 4.33329C8.00021 4.84307 6.53216 5.90762 5.51261 7.31778L6.06653 9.01993L8.30554 9.74693L10.9995 7.78993V5.27293L9.70757 4.33329Z"></path>
                         </svg>
-                        ading . . .
+                        ading 
                     </h1>
                 </div>
             </div>
@@ -58,79 +90,78 @@ const CupPageDetails = () => {
     }
 
     return (
-        // Cup Details Card
-        <>
         <div className="grid sm:grid-cols-1 lg:grid-cols-3 lg:gap-2 relative overflow-x-hidden top-40 p-4">
             <div className="col-span-2 sm:col-span-1 sm:mb-4 lg:col-span-2 mr-3 ml-3">
                 <div className="border rounded-lg bg-gradient-to-r from-slate-100 to-slate-300 bg-opacity-75 p-4 flex items-center">
-                    <div className="flex-none mr-4   lg:bg-white ">
-                    <img
-    src={cupData.league.logo}
-    alt="Club"
-    className="w-16 h-16 lg:w-18 lg:w-19 bg-repeat-space"
-/>
-
+                    <div className="flex-none mr-4 lg:bg-white">
+                        <img
+                            src={cupData.league.logo}
+                            alt="Club"
+                            className="w-16 h-16 lg:w-18 lg:w-19 bg-repeat-space"
+                        />
                     </div>
                     <div className="flex flex-col">
-                        <h2 className="text-xl font-semibold mb-2   bg-gradient-to-r from-slate-900 to-pink-800 bg-clip-text text-transparent">{cupData.league.name}</h2>
+                        <h2 className="text-xl font-semibold mb-2 bg-gradient-to-r from-slate-900 to-pink-800 bg-clip-text text-transparent">{cupData.league.name}</h2>
                         <p>{cupData.country.name}</p>
                         <div className="inline-flex">
-                            <SeasonDropDownlist />
+                            <SeasonDropDownlist onSelectSeason={handleSelectSeason} seasons={seasons} selectedYear={selectedYear}  />
                         </div>
                     </div>
                 </div>
             </div>
 
-
-{/* Featured /Final Score Card */}
-            <div className="col-span-1 sm:col-span-1 lg:col-span-1 mr-3 ml-3 mt-5 md:mt-5 lg:mt-0  bg-gradient-to-r from-rose-100 to-zinc-100 border rounded-lg p-4 shadow-md">
-                <h1 className="text-xl font-bold mb-4  bg-gradient-to-r from-red-700 to-blue-700 bg-clip-text text-transparent">Featured Match</h1>
-                <div className="mb-4">
-                    <p className="text-lg font-semibold">{cupData.league.name}</p>
-                    <p>{cupData.country.name}</p>
-                    <p>Final Match</p>
-                </div>
-                <div className="flex flex-col items-center gap-4">
-                    <div className="flex items-center justify-between w-full">
-                        <div className="flex items-center">
-                            <img src="german_logo.jpg" alt="German Logo" className="w-8 h-8 mr-2" />
-                            <span className="mr-2">German</span>
-                        </div>
-                        <div className="flex items-center">
-                            <span className="mr-2">3</span>
-                            <span className="mr-2 flex justify-center">Vs</span>
-                            <span className="mr-2">2</span>
-                        </div>
-                        <div className="flex items-center">
-                            <img src="newlikand_logo.jpg" alt="Newlikand Logo" className="w-8 h-8 mr-2" />
-                            <span>Newlikand</span>
-                        </div>
+            {/* Featured /Final Score Card */}
+            {matchDetails && (
+                <div className="col-span-1 sm:col-span-1 lg:col-span-1 mr-3 ml-3 mt-5 md:mt-5 lg:mt-0 bg-gradient-to-r from-rose-100 to-zinc-100 border rounded-lg p-4 shadow-md">
+                    <h1 className="text-xl font-bold mb-4 bg-gradient-to-r from-red-700 to-blue-700 bg-clip-text text-transparent">Featured Match</h1>
+                    <div className="mb-4">
+                        <p className="text-lg font-semibold">{cupData.league.name}</p>
+                        <p>{cupData.country.name}</p>
+                        <p>Final Match</p>
                     </div>
-                    <div className="text-gray-500">Finished</div>
+                    <div className="flex flex-col items-center gap-4">
+                        <div className="flex items-center justify-between w-full">
+                            <div className="flex items-center">
+                                <img src={matchDetails.teams.home.logo} alt="Home Team Logo" className="w-8 h-8 mr-2" />
+                                <span className="mr-2">{matchDetails.teams.home.name}</span>
+                            </div>
+                            <div className="flex items-center">
+                                <span className="mr-2">{matchDetails.goals.home}</span>
+                                <span className="mr-2 flex justify-center">Vs</span>
+                                <span className="mr-2">{matchDetails.goals.away}</span>
+                            </div>
+                            <div className="flex items-center">
+                                <img src={matchDetails.teams.away.logo} alt="Away Team Logo" className="w-8 h-8 mr-2" />
+                                <span>{matchDetails.teams.away.name}</span>
+                            </div>
+                        </div>
+                        <div className="text-gray-500">Finished</div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
-
-
-{/* Second Section of the body */}
-        <div>
-        Matches clcik on desktp sttsitics,line ups  page open for mobile ststistics option 
-        by date by group 
-        by round
-        </div>
-
-        {/* third  Section of the body */}
-        <div>
-       events knock out stages
-
-        </div>
-           {/* 4th Section of the body */}
-           <div>
-      add div
-
-        </div>
-        </>
     );
 };
 
 export default CupPageDetails;
+
+
+// {/* 
+// Second Section of the body */}
+//         {/* <div>
+//         Matches clcik on desktp sttsitics,line ups  page open for mobile ststistics option 
+//         by date by group 
+//         by round
+//         </div>
+
+//         {/* third  Section of the body */}
+//         {/* <div>
+//        events knock out stages
+
+//         </div>
+//            {/* 4th Section of the body */}
+          
+   
+
+
+// export default CupPageDetails; 
--- client/src/components/ALLSports/Football/Competitions/Cups/SeasonDropDownlist.js
+++ client/src/components/ALLSports/Football/Competitions/Cups/SeasonDropDownlist.js
@@ -1,8 +1,11 @@
+
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 
-export default function SeasonDropDownlist() {
+export default function SeasonDropDownlist({ onSelectSeason }) {
     const { id } = useParams();
     const [season, setSeason] = useState([]);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -21,6 +24,10 @@ export default function SeasonDropDownlist() {
         setTimeout(() => {
             setLoading(false);
         }, 1000);
+
+        if (onSelectSeason) {
+            onSelectSeason(year);
+        }
     };
 
     const formatDate = (dateString) => {
@@ -89,7 +96,7 @@ export default function SeasonDropDownlist() {
                         aria-haspopup="true"
                         onClick={toggleDropdown}
                     >
-                        {selectedYear ? `${selectedYear}` : 'Select Year'}
+                        {selectedYear ? `${selectedYear}` : 'Latest Season'}
                         <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                         </svg>
