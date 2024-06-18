bb2fc92188c4aef7bf53a8e1ac55d9ec43b9b80f
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-06-18 18:13:57 +0530

    some

--- client/src/components/Home/HomeBody/Blogs.js
+++ client/src/components/Home/HomeBody/Blogs.js
@@ -4,7 +4,7 @@ const Blogs = () => {
   return (
    <>
     Hey Man My blog is here!!!
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit aut deserunt similique laudantium perspiciatis eos repellendus quo! Magnam, nemo at? Consequuntur, consectetur impedit non consequatur vel adipisci soluta provident commodi ullam ipsum vitae quo quasi exercitationem odio tempora facilis quas quod. Eveniet corrupti corporis tenetur? Eaque et perspiciatis laboriosam dicta, praesentium qui facere, quidem expedita, natus sequi tempora?
+    
    </>
   );
 };
