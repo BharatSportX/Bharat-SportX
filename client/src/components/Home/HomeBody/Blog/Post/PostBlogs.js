cf3f06ee50b988818099fbaee98d0c00c59fa488
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-06 23:20:26 +0530

    postblog1

--- client/src/components/Home/HomeBody/Blog/Post/PostBlogs.js
+++ client/src/components/Home/HomeBody/Blog/Post/PostBlogs.js
@@ -3,7 +3,7 @@ import React from "react";
 const PostBlogs = () => {
   return (
     <>
-     <section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+     <section class="bg-gray-200 py-8 antialiased dark:bg-slate-950 md:py-16">
   <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
     <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Shopping Cart</h2>
 
