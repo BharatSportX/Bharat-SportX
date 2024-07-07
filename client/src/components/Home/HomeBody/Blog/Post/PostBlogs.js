dc6845396e8ca0897da0e181754ae4bcadd8f829
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-07 20:39:49 +0530

    struct create

--- client/src/components/Home/HomeBody/Blog/Post/PostBlogs.js
+++ client/src/components/Home/HomeBody/Blog/Post/PostBlogs.js
@@ -1,16 +1,15 @@
 import React from "react";
 import PostStruct from "./PostStruct";
+import { NavLink } from "react-router-dom";
 
 const PostBlogs = () => {
   return (
     <>
       <section className="bg-gray-200 py-1 antialiased dark:bg-slate-950 ">
         <div className="mx-auto max-w-screen-xl px-4 md:px-28 lg:px-0 lg:pr-16  2xl:px-0">
-         
-
           <div className="mt-6 2xl:mr-16 sm:mt-8 md:gap-6 lg:gap-0 lg:flex lg:items-start ">
             <div className="mx-auto lg:mx-0 w-full flex-none lg:max-w-2xl xl:max-w-4xl">
-              <PostStruct/>
+              <PostStruct />
               <div className="hidden xl:mt-8 xl:block">
                 <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">
                   People also bought
@@ -286,95 +285,91 @@ const PostBlogs = () => {
             </div>
 
             <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-              <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-                <p className="text-xl font-semibold text-gray-900 dark:text-white">
-                  Order summary
+              {/* <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6"> */}
+              <div className="space-y-4 rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 ">
+                <p className="text-xl font-semibold px-4 pt-3  text-gray-900 dark:text-white ">
+                  Suggestions for you
                 </p>
-
-                <div className="space-y-4">
-                  <div className="space-y-2">
-                    <dl className="flex items-center justify-between gap-4">
-                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Original price
-                      </dt>
-                      <dd className="text-base font-medium text-gray-900 dark:text-white">
-                        $7,592.00
-                      </dd>
-                    </dl>
-
-                    <dl className="flex items-center justify-between gap-4">
-                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Savings
-                      </dt>
-                      <dd className="text-base font-medium text-green-600">
-                        -$299.00
-                      </dd>
-                    </dl>
-
-                    <dl className="flex items-center justify-between gap-4">
-                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Store Pickup
-                      </dt>
-                      <dd className="text-base font-medium text-gray-900 dark:text-white">
-                        $99
-                      </dd>
-                    </dl>
-
-                    <dl className="flex items-center justify-between gap-4">
-                      <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Tax
-                      </dt>
-                      <dd className="text-base font-medium text-gray-900 dark:text-white">
-                        $799
-                      </dd>
-                    </dl>
+                <hr className=" border-gray-700 m-0 " />
+                <div className="space-y-4 px-4 sm:px-6">
+                  <div className="space-y-3">
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <img
+                          class="w-10 h-10 rounded-full"
+                          src="https://placehold.co/40x40"
+                          alt="User avatar"
+                        />
+                        <div class="ml-4">
+                          <p class="font-semibold">acme_inc</p>
+                          <p class="text-sm text-gray-500">Suggested for you</p>
+                        </div>
+                      </div>
+                      <button class="text-blue-500 hover:text-blue-700">
+                        +
+                      </button>
+                    </div>
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <img
+                          class="w-10 h-10 rounded-full"
+                          src="https://placehold.co/40x40"
+                          alt="User avatar"
+                        />
+                        <div class="ml-4">
+                          <p class="font-semibold">acme_inc</p>
+                          <p class="text-sm text-gray-500">Suggested for you</p>
+                        </div>
+                      </div>
+                      <button class="text-blue-500 hover:text-blue-700">
+                        +
+                      </button>
+                    </div>
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <img
+                          class="w-10 h-10 rounded-full"
+                          src="https://placehold.co/40x40"
+                          alt="User avatar"
+                        />
+                        <div class="ml-4">
+                          <p class="font-semibold">acme_inc</p>
+                          <p class="text-sm text-gray-500">Suggested for you</p>
+                        </div>
+                      </div>
+                      <button class="text-blue-500 hover:text-blue-700">
+                        +
+                      </button>
+                    </div>
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <img
+                          class="w-10 h-10 rounded-full"
+                          src="https://placehold.co/40x40"
+                          alt="User avatar"
+                        />
+                        <div class="ml-4">
+                          <p class="font-semibold">acme_inc</p>
+                          <p class="text-sm text-gray-500">Suggested for you</p>
+                        </div>
+                      </div>
+                      <button class="text-blue-500 hover:text-blue-700">
+                        +
+                      </button>
+                    </div>
                   </div>
 
-                  <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
-                    <dt className="text-base font-bold text-gray-900 dark:text-white">
-                      Total
-                    </dt>
-                    <dd className="text-base font-bold text-gray-900 dark:text-white">
-                      $8,191.00
-                    </dd>
-                  </dl>
+                  
                 </div>
 
-                <a
-                  href="#"
+                <NavLink
+                  to="/"
                   className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Proceed to Checkout
-                </a>
+                  See More
+                </NavLink>
 
-                <div className="flex items-center justify-center gap-2">
-                  <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                    {" "}
-                    or{" "}
-                  </span>
-                  <a
-                    href="#"
-                    title=""
-                    className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
-                  >
-                    Continue Shopping
-                    <svg
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M19 12H5m14 0-4 4m4-4-4-4"
-                      />
-                    </svg>
-                  </a>
-                </div>
+                
               </div>
 
               <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
